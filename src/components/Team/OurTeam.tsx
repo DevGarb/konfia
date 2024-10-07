21ea48a78c86582bea625bc5271a484aa4a65582
Dev Garb <devgarb.fs@gmail.com>
2024-10-07 18:24:48 -0300

    fix:eslint disable-rules

--- src/components/Team/OurTeam.tsx
+++ src/components/Team/OurTeam.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable react/no-unescaped-entities */
+
+
 import React from "react";
 import Image from "next/image";
 import photo from "../../assets/img/mypic.png";
@@ -10,7 +13,7 @@ const OurTeam = () => {
                 <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-ss-lg md:border-e dark:bg-gray-800 dark:border-gray-700">
                     <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
                         <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Very easy this was to integrate</h3>
-                        <p className="my-4">If you care for your time, I hands down would go with this."</p>
+                        <p className="my-4">If you care for your time, I hands down would go with this.</p>
                     </blockquote>
                     <figcaption className="flex items-center justify-center ">
                         <Image className="rounded-full w-9 h-9" src={photo} alt="profile picture"/>
