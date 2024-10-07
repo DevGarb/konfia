70529737742d21fc4bfc505178662cb2fb420796
Dev Garb <devgarb.fs@gmail.com>
2024-10-07 18:00:13 -0300

    fix:class tailwind

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,17 +5,17 @@ import photo from "../../assets/img/mypic.png";
 const Hero1 = () => {
     return (
         <>
-            <div className="flex items-center flex-wrap">
+            <div className="flex items-center flex-wrap my-10">
                 <div className="mx-auto max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8">
                     <div className="max-w-2xl">
-                        <h2 className="text-3xl font-bold tracking-tight mt-7 mb-14 text-gray-900 sm:text-4xl">
+                        <h2 className="text-3xl font-bold tracking-tight mt-7 mb-14 text-gray-700 sm:text-4xl">
                             Conheça Essa Nova Experiência!
                         </h2>
                         {/* <p className="mt-6 text-lg leading-8 text-gray-600">
                             Libero fames augue nisl porttitor nisi, quis. Id ac elit odio
                             vitae elementum enim vitae ullamcorper suspendisse.
                         </p> */}
-                        <h6 className="my-6 text-justify text-lg leading-8 font-semibold text-gray-500 lg:text-xl dark:text-gray-400 bg-slate-200 max-lg:w-screen px-4">
+                        <h6 className="my-6 text-justify text-lg leading-8 font-semibold text-gray-500 lg:text-xl dark:text-gray-400 bg-white bg-opacity-25 shadow-xl px-4">
                             <p>
                                 Chegou a solução perfeita para quem quer praticidade e segurança!
                             </p>
@@ -55,13 +55,13 @@ const Hero1 = () => {
                                             xmlns="http://www.w3.org/2000/svg"
                                             fill="none"
                                             viewBox="0 0 24 24"
-                                            stroke-width="1.5"
+                                            strokeWidth="1.5"
                                             stroke="currentColor"
                                             className="w-6 h-6"
                                         >
                                             <path
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
                                                 d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z"
                                             />
                                         </svg>
@@ -128,9 +128,9 @@ const Hero1 = () => {
                                             className="h-5 w-5 text-yellow-400"
                                         >
                                             <path
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
-                                                stroke-width="2"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
                                                 d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
                                             ></path>
                                         </svg>
@@ -148,18 +148,18 @@ const Hero1 = () => {
                                             >
                                                 <g
                                                     fill="none"
-                                                    fill-rule="nonzero"
+                                                    fillRule="nonzero"
                                                     stroke="none"
-                                                    stroke-width="1"
-                                                    stroke-linecap="butt"
-                                                    stroke-linejoin="miter"
-                                                    stroke-miterlimit="10"
-                                                    stroke-dasharray=""
-                                                    stroke-dashoffset="0"
-                                                    font-family="none"
-                                                    font-weight="none"
-                                                    font-size="none"
-                                                    text-anchor="none"
+                                                    strokeWidth="1"
+                                                    strokeLinecap="butt"
+                                                    strokeLinejoin="miter"
+                                                    strokeMiterlimit="10"
+                                                    strokeDasharray=""
+                                                    strokeDashoffset="0"
+                                                    fontFamily="none"
+                                                    fontWeight="none"
+                                                    fontSize="none"
+                                                    textAnchor="none"
                                                     className="mix-blend-mode: normal"
                                                 >
                                                     <path d="M0,172v-172h172v172z" fill="none"></path>
@@ -194,18 +194,18 @@ const Hero1 = () => {
                                             >
                                                 <g
                                                     fill="none"
-                                                    fill-rule="nonzero"
+                                                    fillRule="nonzero"
                                                     stroke="none"
-                                                    stroke-width="1"
-                                                    stroke-linecap="butt"
-                                                    stroke-linejoin="miter"
-                                                    stroke-miterlimit="10"
-                                                    stroke-dasharray=""
-                                                    stroke-dashoffset="0"
-                                                    font-family="none"
-                                                    font-weight="none"
-                                                    font-size="none"
-                                                    text-anchor="none"
+                                                    strokeWidth="1"
+                                                    strokeLinecap="butt"
+                                                    strokeLinejoin="miter"
+                                                    strokeMiterlimit="10"
+                                                    strokeDasharray=""
+                                                    strokeDashoffset="0"
+                                                    fontFamily="none"
+                                                    fontWeight="none"
+                                                    fontSize="none"
+                                                    textAnchor="none"
                                                     className="mix-blend-mode: normal"
                                                 >
                                                     <path d="M0,172v-172h172v172z" fill="none"></path>
@@ -237,9 +237,9 @@ const Hero1 = () => {
                                             fill="currentColor"
                                         >
                                             <path
-                                                fill-rule="evenodd"
+                                                fillRule="evenodd"
                                                 d="M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z"
-                                                clip-rule="evenodd"
+                                                clipRule="evenodd"
                                             ></path>
                                         </svg>
                                         <p className="">7 Projetos</p>
